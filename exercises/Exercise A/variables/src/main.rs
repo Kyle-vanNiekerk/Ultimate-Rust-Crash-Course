d8094ec6948883e5250649a074c154715eadfb14
Kyle van Niekerk <kylevn185@gmail.com>
2025-11-10 10:11:53 +0200

    Complete Excercise A

--- exercises/Exercise A/variables/src/main.rs
+++ exercises/Exercise A/variables/src/main.rs
@@ -2,10 +2,11 @@ const STARTING_MISSLES: i32 = 8;
 const READY_MISSLES: i32 = 2;
 
 fn main() {
-    let mut missles: i32 = STARTING_MISSLES;
-    let ready: i32 = READY_MISSLES;
+    let (ready, mut missles): (i32,i32) = (READY_MISSLES,STARTING_MISSLES);
 
     println!("Firing {} of my {} missles...", ready,missles);
     missles = missles - ready;
     println!("{} missles left", missles);
+
+   // println!("{} missles left", missles-ready);
 }
